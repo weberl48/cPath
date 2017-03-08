ffdf8ae732f75f0e97c0734402509b45138bb018
Lucas Weber <lweber48@live.com>
2017-03-08 14:27:52 -0500

    console minutes tracker in c#

---
+++ FitnessApp/FitnessApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace FitnessApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool keepGoing = true;
+            int runningTotal = 0;
+            while (keepGoing)
+            {
+                Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+                string minutesEntry = Console.ReadLine();
+                if (minutesEntry == "quit")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    int minutes = int.Parse(minutesEntry);
+                    runningTotal +=  minutes;
+                    Console.WriteLine("You've worked out " + runningTotal + " minutes");
+                }
+            }
+
+        }
+    }
+}
