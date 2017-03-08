469287b2b14582ed40181bf97c8b7f027e695cbc
Lucas Weber <375474@wegmans.com>
2017-03-08 09:25:33 -0500

    Add project files.

---
+++ GradeBook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(33);
+            book.AddGrade(87);
+            book.AddGrade(66);
+            book.AddGrade(75);
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.Read();
+        }
+    }
+}
