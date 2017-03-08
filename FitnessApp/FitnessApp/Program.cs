ce295a95a7dd84f3a15c04d2d1d0939e4d6ce908
Lucas Weber <lweber48@live.com>
2017-03-08 15:39:00 -0500

    try and catch exception added for minutes format

--- FitnessApp/FitnessApp/Program.cs
+++ FitnessApp/FitnessApp/Program.cs
@@ -17,9 +17,39 @@ namespace FitnessApp
                 }
                 else
                 {
-                    int minutes = int.Parse(minutesEntry);
-                    runningTotal +=  minutes;
+                   
+                    try
+                    {
+                        int minutes = int.Parse(minutesEntry);
+                        if (minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value.");
+                        }
+                        else if (minutes <= 10)
+                        {
+                            Console.WriteLine("Better than nothing, am I right?");
+                        }
+                        else if (minutes <= 30)
+                        {
+                            Console.WriteLine("Way to go!");
+                        }
+                        else if (minutes <= 60)
+                        {
+                            Console.WriteLine("You must be a ninja warrior in training!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Okay, now you're just showing off!");
+                        }
+                        runningTotal += minutes;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("That is not valid input.");
+                        continue;
+                    }
                     Console.WriteLine("You've worked out " + runningTotal + " minutes");
+
                 }
             }
 
