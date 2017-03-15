0869fe91f66921446c988cfdfc95d1790b0c42b6
Lucas Weber <weberl48@outlook.com>
2017-03-15 13:50:16 -0400

    Inital setup

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+
+            var movie = new Movie()
+            {
+                Name = "Sharknado"
+            };
+            return View(movie);
+        }
+    }
+}
